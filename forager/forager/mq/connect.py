bc9a7be5106a66c7eb6b9dd52ea893dbc1946299
Florian Adomeit <florian@elsewhen.com>
2021-08-18 10:12:40 +0100

    fix(forager): user url parameters to open connection

--- forager/forager/mq/connect.py
+++ forager/forager/mq/connect.py
@@ -3,12 +3,8 @@ import os
 
 def connect():
     param = os.environ.get("CLOUDAMQP_URL")
-    param2 = os.environ.get("CLOUDAMQP")
     if not param:
-        param = "localhost"
-    
-    print("1: %s" % param)
-    print("2: %s" % param2)
-    connection = pika.BlockingConnection(pika.ConnectionParameters(param))
+        param = "amqp://guest:guest@localhost"
+    connection = pika.BlockingConnection(pika.URLParameters(param))
 
     return connection.channel(), connection
