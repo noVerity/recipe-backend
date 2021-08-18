91e2551bb80866f1df6ff1ad4142ae7890d45aa9
Florian Adomeit <florian@elsewhen.com>
2021-08-18 10:01:41 +0100

    chore: debug message queue credentials

--- forager/forager/mq/connect.py
+++ forager/forager/mq/connect.py
@@ -3,8 +3,12 @@ import os
 
 def connect():
     param = os.environ.get("CLOUDAMQP_URL")
+    param2 = os.environ.get("CLOUDAMQP")
     if not param:
         param = "localhost"
+    
+    print("1: %s" % param)
+    print("2: %s" % param2)
     connection = pika.BlockingConnection(pika.ConnectionParameters(param))
 
     return connection.channel(), connection
