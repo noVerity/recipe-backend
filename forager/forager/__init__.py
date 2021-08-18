55584aa435c0ce59e68be6c4aa47a0abdfb77831
Florian Adomeit <florian@elsewhen.com>
2021-08-18 09:35:00 +0100

    fix(forager): handle message queue in daemon

--- forager/forager/__init__.py
+++ forager/forager/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
-import asyncio
+from werkzeug.serving import is_running_from_reloader
+import threading
 from forager.mq.send import send
 from forager.mq.receive import receive
 __version__ = '0.1.0'
@@ -10,6 +11,9 @@ def create_app():
     from forager.views import index
     app.register_blueprint(index)
 
-    asyncio.run(receive())
+    if not is_running_from_reloader():
+        receivingThread = threading.Thread(target=receive)
+        receivingThread.daemon = True
+        receivingThread.start()
 
     return app
