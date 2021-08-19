aea2b5829bdd42e2c4fbed98e2ee30ad41c1409d
Florian Adomeit <florian@elsewhen.com>
2021-08-19 09:47:00 +0100

    feat(forager): flesh out tests

--- forager/forager/views.py
+++ forager/forager/views.py
@@ -1,8 +1,13 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, url_for, current_app
 index = Blueprint('index', __name__)
 from forager.fooddata.lookup import lookup
 import os
 
+
+def has_no_empty_params(rule):
+    defaults = rule.defaults if rule.defaults is not None else ()
+    arguments = rule.arguments if rule.arguments is not None else ()
+    return len(defaults) >= len(arguments)
 
 @index.route("/lookup")
 def Echoer():
@@ -25,3 +30,16 @@ def Echoer():
         return lookup(item)
     except IOError:
         return { "error": "could not retrieve url" }
+
+
+@index.route("/site-map")
+def site_map():
+    links = []
+    for rule in current_app.url_map.iter_rules():
+        # Filter out rules we can't navigate to in a browser
+        # and rules that require parameters
+        if "GET" in rule.methods and has_no_empty_params(rule):
+            url = url_for(rule.endpoint, **(rule.defaults or {}))
+            links.append((url, rule.endpoint))
+    # links is now a list of url, endpoint tuples
+    return { "links": links }
--- forager/tests/test_forager.py
+++ forager/tests/test_forager.py
@@ -1,5 +1,20 @@
-from forager import __version__
+import unittest
+from unittest.mock import patch
+from json import loads
+from forager import __version__, create_app
 
+class ForagerTestCase(unittest.TestCase):
 
-def test_version():
-    assert __version__ == '0.1.0'
+    def test_version(self):
+        assert __version__ == '0.1.0'
+    
+    @patch('forager.is_running_from_reloader', lambda: True)
+    def test_available_routes(self):
+        with create_app().test_client() as c:
+            response = c.get('/site-map')
+            self.assertEqual(response.status_code, 200)
+            result = loads(response.data)
+            assert [
+            "/lookup", 
+            "index.Echoer"
+            ] in result.get("links")
