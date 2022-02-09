c4842ea74957a5f199e3c346f645dc27fc25c0d4
Florian Adomeit <florian@elsewhen.com>
2022-02-09 16:26:00 +0000

    fix: proxy to update the requested host

--- gateway/proxy.go
+++ gateway/proxy.go
@@ -16,16 +16,14 @@ func ReverseProxy(target *url.URL) gin.HandlerFunc {
 func ReverseRewriteProxy(target *url.URL, rewritePath func(string) string) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		director := func(req *http.Request) {
-			//r := c.Request
-			//req = r
 			copy := target
 			copy.Path = rewritePath(req.URL.Path)
 			req.URL = copy
-			// req.Header["my-header"] = []string{r.Header.Get("my-header")}
-			// Golang camelcases headers
-			//delete(req.Header, "My-Header")
+			req.Host = copy.Host
 		}
-		proxy := &httputil.ReverseProxy{Director: director}
+		proxy := &httputil.ReverseProxy{
+			Director: director,
+		}
 		proxy.ServeHTTP(c.Writer, c.Request)
 	}
 }
--- gateway/user.go
+++ gateway/user.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"net/url"
 
 	"github.com/gin-gonic/gin"
 )
 
 func SetupUserService(router *gin.Engine, userService *url.URL) {
-	router.Any("/user", ReverseProxy(userService))
+	router.Any("/user", ReverseProxy(userService), func(c *gin.Context) {
+		fmt.Printf("HERE: %v\n", c.Request.URL.Host)
+	})
 	router.Any("/login", ReverseProxy(userService))
 }
