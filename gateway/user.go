8f73c814afff3453fe999e490fee3fe1522e04c3
Florian Adomeit <florian@elsewhen.com>
2022-02-09 16:35:24 +0000

    chore: remove debug line

--- gateway/user.go
+++ gateway/user.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"net/url"
 
 	"github.com/gin-gonic/gin"
 )
 
 func SetupUserService(router *gin.Engine, userService *url.URL) {
-	router.Any("/user", ReverseProxy(userService), func(c *gin.Context) {
-		fmt.Printf("HERE: %v\n", c.Request.URL.Host)
-	})
+	router.Any("/user", ReverseProxy(userService))
 	router.Any("/login", ReverseProxy(userService))
 }
